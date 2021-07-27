15572018c72801a71f2b3270e658f8cea8e0e9dc
joraavanes <jora_a@outlook.com>
2021-07-27 17:51:06 +0430

    Seperate apolloServer initializer into a new file

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,20 +1,16 @@
 import express, { Application, NextFunction, Request, Response} from 'express'
-import { ApolloServer } from 'apollo-server-express'
+import { initializeApolloServer } from './graphql/apolloServer';
 
 const PORT: Number = Number(process.env.PORT) || 5000;
 
-const initializeServer = ():void => {
-    const apolloServer = new ApolloServer({});
+const initializeServer = async (): Promise<void> => {
     const app: Application = express();
     
     app.get('/', (req: Request, res: Response, next: NextFunction) => {
         res.send('Hello ts express. Welcome to typescript express');
     });
 
-    apolloServer.applyMiddleware({
-        app,
-        path: '/graphql'
-    });
+    await initializeApolloServer(app);
 
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 };
