fd41a47b59e27f11f5b22643c63e9131572acdd8
joraavanes <jora_a@outlook.com>
2021-07-27 15:16:02 +0430

    Add apollo server to express app

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,24 @@
 import express, { Application, NextFunction, Request, Response} from 'express'
-
-const app: Application = express();
+import { ApolloServer } from 'apollo-server-express'
 
 const PORT: Number = Number(process.env.PORT) || 5000;
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello ts express. Welcome to typescript express');
-});
+const initializeServer = ():void => {
+    const apolloServer = new ApolloServer({});
+    const app: Application = express();
+    
+    app.get('/', (req: Request, res: Response, next: NextFunction) => {
+        res.send('Hello ts express. Welcome to typescript express');
+    });
 
-app.listen(PORT, () => console.log('Server is running on http://localhost:5000'));
+    apolloServer.applyMiddleware({
+        app,
+        path: '/graphql'
+    });
+
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+};
+
+initializeServer();
+
+
