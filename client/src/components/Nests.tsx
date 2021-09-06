46e4cf174ed1bafcc79f5a0f2a4111dd48e04363
joraavanes <jora_a@outlook.com>
2021-09-06 14:47:58 +0430

    Add Link on nests that points its seperate page - remove types from Nests.tsx

--- client/src/components/Nests.tsx
+++ client/src/components/Nests.tsx
@@ -1,72 +1,41 @@
 import React, { useEffect } from 'react';
-import { useQuery, gql } from '@apollo/client';
-
-const GET_NESTS = gql`
-    query {
-        nests {
-            
-            title
-            price
-            furnished
-            dryer
-            latitude
-            longitude
-        }
-    }
-`;
-
-interface Nest {
-    _id: string,
-    title: string,
-    type: string,
-    size: number,
-    furnished: boolean,
-    dishwasher: boolean,
-    washingMachine: boolean,
-    dryer: boolean,
-    airConditioning: boolean,
-    heating: boolean,
-    rooms: number,
-    parking: number,
-    longitude: number,
-    latitude: number,
-    price: number,
-    photos: string[],
-}
-
-interface GetNestsData {
-    nests: Nest[];
-    loading: boolean;
-}
+import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom'
+import { GetNestsData } from '../types';
+import { GET_NESTS } from '../graphql/queries';
 
 const Nests: React.FC = () => {
-    const { loading, error, data } = useQuery(GET_NESTS);
+    const { loading, error, data } = useQuery<GetNestsData>(GET_NESTS);
 
     useEffect(() => {
         console.log(data);
-        console.log(error);
+        console.log(error?.message);
 
         return () => {
             
         }
-    }, [loading]);
+    }, [loading, error]);
 
     return (
         <div>
             <h2>Nests</h2>
             {loading && <span>Loading ...</span>}
-            <pre>
-                {data && data.nests && JSON.stringify(data.nests, undefined, 4)}
+                {/* {data && data.nests && JSON.stringify(data.nests, undefined, 4)} */}
                 {data && data.nests && data.nests.map(nest => (
-                    <>
+                    <div key={nest._id}>
+                        <div>{nest._id}</div>
                         <div>{nest.title}</div>
                         <div>{nest.price}</div>
                         <div>{nest.latitude}</div>
                         <div>{nest.longitude}</div>
+                        <Link to={{
+                            pathname: `/nest/${nest._id}`
+                        }}>
+                            Checkout
+                        </Link>
                         <hr />
-                    </>
+                    </div>
                 ))}
-            </pre>
         </div>
     );
 };
