a76eae14f537dc9d24ddfdd3944e3ce1170fb3f9
joraavanes <jora_a@outlook.com>
2021-09-12 10:44:58 +0430

    Add Dashboard component for CRUD functions

---
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { useQuery } from '@apollo/client'
+import { GET_NESTS } from '../../graphql/queries';
+import { GetNestsData } from '../../types';
+
+const Dashboard = () => {
+    const {data, loading, error} = useQuery<GetNestsData>(GET_NESTS);
+
+    return (
+        <div>
+            <NavLink to="/dashboard/AddNest">Add Nest</NavLink>
+            {error && <p>Failed getting the nests</p>}
+            {data && data.nests.map(nest => (
+                <div>
+                    <p>{nest.title} - {nest.price} - <NavLink to={`/EditNest/${nest._id}`}>Edit</NavLink></p>
+                </div>
+            ))}
+        </div>
+    )
+};
+
+export default Dashboard;
