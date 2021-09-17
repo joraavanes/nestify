1f8f8dc17eb3c35932486738d386d763c17f509a
joraavanes <jora_a@outlook.com>
2021-09-17 23:56:16 +0430

    Export express loader to use for mock testing

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,14 +1,16 @@
 import colors from 'colors';
+import { Application } from 'express';
 import { expressLoader } from './loaders/express';
 import config from './config';
 
-const startServer = async () => {
+export const startServer = async (): Promise<Application> => {
     const app = await expressLoader();
 
     app.listen(
         config.port,
         () => console.log(colors.bgGreen(colors.black(` Server is running on port ${config.port} `))),
     );
+    return app;
 };
 
 startServer();
