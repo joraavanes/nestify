bc1ee7537a44c09b6754abfca6a38c40aa8204c8
joraavanes <jora_a@outlook.com>
2021-09-22 23:37:09 +0330

    Move mongoLoader & apolloServer from expressLoader to startServer

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,16 +1,23 @@
 import colors from 'colors';
-import { Application } from 'express';
-import { expressLoader } from './loaders/express';
 import config from './config';
+import { expressLoader } from './loaders/express';
+import { mongoLoader } from './loaders/mongo';
+import { initializeApolloServer } from './loaders/apolloServer';
 
-export const startServer = async (): Promise<Application> => {
+export const startServer = async (): Promise<void> => {
     const app = await expressLoader();
-
     app.listen(
         config.port,
         () => console.log(colors.bgGreen(colors.black(` Server is running on port ${config.port} `))),
     );
-    return app;
+
+    try {
+        await mongoLoader();
+    } catch (error) {
+        console.log(colors.bgRed(error.message));
+    }
+
+    await initializeApolloServer(app);
 };
 
 startServer();
--- api/src/loaders/express.ts
+++ api/src/loaders/express.ts
@@ -6,22 +6,16 @@ import express, {
     NextFunction,
 } from 'express';
 
-import { initializeApolloServer } from './apolloServer';
-import { mongoLoader } from './mongo';
-
 export const expressLoader = async (): Promise<Application> => {
     const app: Application = express();
+
     app.get('/', (req: Request, res: Response, next: NextFunction) => {
-        res.send('Hello ts express. Welcome to typescript express');
+        res.status(200).send('Hello ts express. Welcome to typescript express');
     });
 
-    try {
-        await mongoLoader();
-    } catch (error) {
-        console.log(colors.bgRed(error.message));
-    }
-
-    await initializeApolloServer(app);
+    app.get('/info', (req: Request, res: Response, next: NextFunction) => {
+        res.status(200).send('info');
+    });
 
     return app;
 };
