c8c7dccda77f195369e66710fa69333a6fe80c9f
joraavanes <jora_a@outlook.com>
2021-11-06 15:36:29 +0330

    Covert user login query into a mutation

--- api/src/graphql/mutation.ts
+++ api/src/graphql/mutation.ts
@@ -8,6 +8,8 @@ import {
     GraphQLString,
 } from 'graphql';
 import { NestModel, NestType } from '../entity/nest';
+import { LoginType, UserType } from '../entity/user';
+import { UserService } from '../services/UserService';
 
 export const mutation = new GraphQLObjectType({
     name: 'mutation',
@@ -93,6 +95,40 @@ export const mutation = new GraphQLObjectType({
                 );
                 return result;
             },
+        },
+        registerUser: {
+            type: UserType,
+            args: {
+                email: { type: GraphQLNonNull(GraphQLString) },
+                password: { type: GraphQLNonNull(GraphQLString), },
+                name: { type: GraphQLNonNull(GraphQLString) },
+                surname: { type: GraphQLNonNull(GraphQLString) },
+                userConfirmed: { type: GraphQLBoolean },
+            },
+            async resolve(source, {email, password, name, surname, userConfirmed}) {
+                const user = await UserService.createUser({email, password, name, surname, userConfirmed});
+                return user;
+            },
+        },
+        login: {
+            type: LoginType,
+            args: {
+                username: { type: GraphQLNonNull(GraphQLString) },
+                password: { type: GraphQLNonNull(GraphQLString) },
+            },
+            async resolve(source, { username, password }){
+                const token = await UserService.loginUser(username, password);
+
+                if(token){
+                    return {
+                        result: 'success',
+                        description: 'Logged in successfully',
+                        token
+                    }
+                }
+
+                return { result: 'Login Failed', description: 'Username or password is incorrect.' };
+            }
         },
     },
 });
--- api/src/graphql/query.ts
+++ api/src/graphql/query.ts
@@ -1,6 +1,6 @@
 import { GraphQLID, GraphQLList, GraphQLObjectType, GraphQLString, GraphQLNonNull } from 'graphql';
 import { NestModel, NestType } from '../entity/nest';
-import { LoginType, UserType } from '../entity/user';
+import { UserType } from '../entity/user';
 import { UserService } from '../services/UserService';
 
 export const query = new GraphQLObjectType({
@@ -43,26 +43,6 @@ export const query = new GraphQLObjectType({
                 return id ? 
                     UserService.getUserById(id):
                     UserService.getUserByEmail(email);
-            }
-        },
-        login: {
-            type: LoginType,
-            args: {
-                username: { type: GraphQLNonNull(GraphQLString) },
-                password: { type: GraphQLNonNull(GraphQLString) },
-            },
-            async resolve(source, { username, password }){
-                const token = await UserService.loginUser(username, password);
-
-                if(token){
-                    return {
-                        result: 'success',
-                        description: 'Logged in successfully',
-                        token
-                    }
-                }
-
-                return { result: 'Login Failed', description: 'Username or password is incorrect.' };
             }
         },
     },
