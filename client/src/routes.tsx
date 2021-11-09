880ce10854c658abb316fe0845fb8279ac2700b0
joraavanes <jora_a@outlook.com>
2021-11-10 01:50:00 +0330

    Add register route

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 const Login = lazy(() => import('./components/Auth/Login'));
+const Register = lazy(() => import('./components/Auth/Register'));
 const App = lazy(() => import('./components/App'));
 const Nest = lazy(() => import('./components/Nest'));
 const Nests = lazy(() => import( './components/Nests'));
@@ -20,6 +21,7 @@ export const Routes: React.FC = () => (
                 <Route path="/dashboard/addnest" component={AddNest} exact={true}/>
                 <Route path="/dashboard/EditNest/:id" component={EditNest} exact={true}/>
                 <Route path="/login" component={Login} exact={true}/>
+                <Route path="/register" component={Register} exact={true}/>
             </Switch>
         </Suspense>
     </Router>
