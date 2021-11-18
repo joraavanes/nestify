f04b0f3bc3c07cd089cf499443b6055a0c80b883
joraavanes <jora_a@outlook.com>
2021-11-18 18:24:47 +0330

    Refetch the query in Dashboard after editing & removing

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { useMutation, useQuery } from '@apollo/client'
 import { GET_NESTS } from '../../graphql/queries';
@@ -6,9 +7,16 @@ import { GetNestData, GetNestsData, NestRemoveTVariables } from '../../types';
 import { REMOVE_NEST } from '../../graphql/mutations';
 
 const Dashboard = () => {
-    const {data, loading, error} = useQuery<GetNestsData>(GET_NESTS);
+    const location = useLocation()
+    const {data, loading, error, refetch} = useQuery<GetNestsData>(GET_NESTS);
 
     const [RemoveNest , {data:removeData, loading:removeLoading, error: removeError}] = useMutation<GetNestData,NestRemoveTVariables>(REMOVE_NEST);
+    
+    useEffect(() => {
+        refetch();
+        
+        return () => {};
+    }, [location.pathname, removeData]);
 
     return (
         <div>
