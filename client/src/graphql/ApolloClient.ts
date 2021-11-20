cb55607ac25ca7824d27d90fd500d9c7c5d98eef
joraavanes <jora_a@outlook.com>
2021-11-20 11:30:43 +0330

    Change apolloClient link

--- client/src/graphql/ApolloClient.ts
+++ client/src/graphql/ApolloClient.ts
@@ -2,5 +2,5 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: createHttpLink({uri: 'http://localhost:5000/api'})
+    link: createHttpLink({uri: '/api'}) // localhost:5000/api if client runs on devServer
 });
