0c8c12561a30bc5a19a5f07b9c2d3af1875b8590
joraavanes <jora_a@outlook.com>
2021-11-21 00:55:18 +0330

    Add addBooking to BookingService

--- api/src/services/BookingService.ts
+++ api/src/services/BookingService.ts
@@ -1,5 +1,7 @@
 import { ObjectId } from 'mongodb';
+import { UserService } from '.';
 import { Booking, BookingModel } from '../entity/booking';
+import { NestModel } from '../entity/nest';
 
 export class BookingService{
     static async getBookings(): Promise<Booking[]> {
@@ -8,5 +10,28 @@ export class BookingService{
 
     static async getBookingById(id: string): Promise<Booking> {
         return BookingModel.find({_id: new ObjectId(id)});
+    }
+
+    static async addBooking(tenantId: string, nestId: string, checkIn: number, checkout?: number): Promise<Booking>{
+        try {
+            const tenant = await UserService.getUserById(tenantId);
+            const nest = await NestModel.findOne({_id: new ObjectId(nestId)});
+
+            if(!tenant || !nest){
+                throw new Error('Failed to add booking');
+            }
+
+            const model = new BookingModel({
+                tenant: tenant._id,
+                nest: nest._id,
+                checkIn: new Date(checkIn),
+            });
+
+            return await model.save();
+
+        } catch (error) {
+            // todo: log error
+            return Promise.reject('Failed to add Booking');
+        }
     }
 }
