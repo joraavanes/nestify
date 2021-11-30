c3b3a70e146e3cb037f2ec5673c3392d3ad43ac6
joraavanes <jora_a@outlook.com>
2021-11-30 11:22:28 +0330

    Add webpack config for server app

---
+++ webpack.server.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const nodeExternals = require('webpack-node-externals');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    target: 'node',
+    entry: './api/src/app.ts',
+    mode: 'production',
+    externalsPresets: { node: true },
+    context: __dirname,
+
+    externals: [nodeExternals()],
+    output: {
+        path: path.join(__dirname, 'api/dist'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'ts-loader',
+                test: /\.ts$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'api/src/config/.env' },
+                { from: 'api/public/', to: 'public/' },
+            ]
+        })
+    ],
+    resolve:{
+        extensions: ['.ts','.js', '.json']
+    }
+};
