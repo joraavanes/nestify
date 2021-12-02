4a622c1319c88d77e448a443d40a296e87e10e94
joraavanes <jora_a@outlook.com>
2021-12-02 23:26:55 +0330

    Add UpdateNest interface

--- api/src/entity/nest.ts
+++ api/src/entity/nest.ts
@@ -33,6 +33,25 @@ export const NestType = new GraphQLObjectType({
     }),
 });
 
+export interface UpdateNest {
+    title?: string,
+    type?: string,
+    size?: number,
+    furnished?: boolean,
+    dishwasher?: boolean,
+    washingMachine?: boolean,
+    dryer?: boolean,
+    airConditioning?: boolean,
+    heating?: boolean,
+    rooms?: number,
+    parking?: number,
+    longitude?: number,
+    latitude?: number,
+    price?: number,
+    photos?: string[],
+    landlord?: string,
+}
+
 export interface Nest {
     title: string,
     type: string,
