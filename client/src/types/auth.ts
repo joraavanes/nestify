5d7bb8c67906f6a90352378d0dae388f56a31a4f
joraavanes <jora_a@outlook.com>
2021-12-11 11:29:30 +0330

    Refine types for login response data

--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -1,5 +1,9 @@
 export interface LoginData {
-    result: string;
+    login:{ 
+        result: string,
+        description: string,
+        token: string,
+    };
     loading: boolean;
     error: Error;
 }
