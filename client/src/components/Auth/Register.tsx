c8567e0b28ae40b3776cc0e1cc114c119116ac5c
Jora <jora_a@outlook.com>
2021-12-15 20:26:29 +0330

    Validate input in Register using formik & yup

--- client/src/components/Auth/Register.tsx
+++ client/src/components/Auth/Register.tsx
@@ -1,31 +1,41 @@
 import { useMutation } from '@apollo/client';
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useEffect } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { REGISTER_USER } from '../../graphql/mutations';
 import { RegisterData, RegisterVariables } from '../../types';
 
 const Register = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
-    const [surname, setSurname] = useState('');
-
     const [RegisterUser, {data, loading, error}] = useMutation<RegisterData, RegisterVariables>(REGISTER_USER);
 
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
+    const { errors, touched, handleSubmit, getFieldProps } = useFormik({
+        initialValues:{
+            email: '',
+            password: '',
+            name: '',
+            surname: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().email('Email address is not valid. Please check it out').max(50, 'Atmost 50 characters for email is accepted').required(),
+            password: Yup.string().min(6, 'Atleast 6 characters for password is needed').required(),
+            name: Yup.string().max(25, 'Name is limited to 25 characters').required('Name is required'),
+            surname: Yup.string().max(25, 'Surname is limited to 25 characters').required('Surname is required'),
+        }),
+        onSubmit: values => {
+            alert(JSON.stringify(values, undefined, 2));
 
-        console.log(email, password, name, surname);
+            RegisterUser({
+                variables: {
+                    email: values.email,
+                    password: values.password,
+                    name: values.name,
+                    surname: values.surname
+                }
+            });
+        },
+    });
 
-        RegisterUser({
-            variables: {
-                email,
-                password,
-                name,
-                surname
-            }
-        });
-        console.log(data);
-    };
+    // const handleSubmit = (e: FormEvent) =>  e.preventDefault();
 
     useEffect(() => {
         console.log(data);
@@ -40,19 +50,23 @@ const Register = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="email">Email:</label>
-                    <input type="text" name="email" id="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input type="text" id="email" {...getFieldProps('email')}/>
+                    {errors.email && touched.email && <span>{errors.email}</span>}
                 </div>
                 <div>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                    <input type="password" id="password" {...getFieldProps('password')}/>
+                    {errors.password && touched.password && <span>{errors.password}</span>}
                 </div>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" id="name" value={name} onChange={e => setName(e.target.value)}/>
+                    <input type="text" id="name" {...getFieldProps('name')}/>
+                    {errors.name && touched.name && <span>{errors.name}</span>}
                 </div>
                 <div>
                     <label htmlFor="surname">Surname:</label>
-                    <input type="text" name="surname" id="surname" value={surname} onChange={e => setSurname(e.target.value)}/>
+                    <input type="text" id="surname" {...getFieldProps('surname')}/>
+                    {errors.surname && touched.surname && <span>{errors.surname}</span>}
                 </div>
                 <div>
                     <input type="submit" value="Register" />
