e2eef8913dfdee0c81f343dd8b67c02d9857dade
Jora <jora_a@outlook.com>
2021-12-18 13:24:16 +0330

    Disable apollo client cache

--- client/src/graphql/ApolloClient.ts
+++ client/src/graphql/ApolloClient.ts
@@ -1,6 +1,18 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
+const defaultOptions: any = {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+};
+
 export const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: createHttpLink({uri: '/api'}) // localhost:5000/api if client runs on devServer
+    link: createHttpLink({uri: '/api'}), // localhost:5000/api if client runs on devServer
+    defaultOptions,
 });
