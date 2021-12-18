22fbf6e250cf9f6b4715be7695385bed3f5aa721
Jora <jora_a@outlook.com>
2021-12-18 22:20:13 +0330

    Apply formik in NestForm

--- client/src/components/Dashboard/NestForm.tsx
+++ client/src/components/Dashboard/NestForm.tsx
@@ -1,4 +1,6 @@
-import React, { FormEvent, useState } from 'react'
+import React, { FormEvent, useState } from 'react';
+import { Form, Formik, FormikProps, useField, useFormik } from 'formik';
+import * as Yup from 'yup';
 import { Nest } from '../../types';
 
 interface NestFormProps {
@@ -6,118 +8,140 @@ interface NestFormProps {
     nest?: Nest;
 };
 
-const NestForm: React.FC<NestFormProps> = ({handleNestMutation, nest}) => {
+interface HtmlInputProps{
+    label?: string;
+    type?: string;
+    name: string;
+    id: string;
+};
 
-    const [id, setId] = useState(nest ? nest._id : '');
-    const [title, setTitle] = useState(nest ? nest.title : '');
-    const [type, setType] = useState(nest ? nest.type : '');
-    const [size, setSize] = useState<number>(nest ? nest.size : 0);
-    const [furnished, setFurnished] = useState<boolean>(nest ? nest.furnished : false);
-    const [dishwasher, setDishwasher] = useState<boolean>(nest ? nest.dishwasher : false);
-    const [washingMachine, setWashingMachine] = useState<boolean>(nest ? nest.washingMachine : false);
-    const [dryer, setDryer] = useState<boolean>(nest ? nest.dryer : false);
-    const [airConditioning, setAirconditioning] = useState<boolean>(nest ? nest.airConditioning : false);
-    const [heating, setHeating] = useState<boolean>(nest ? nest.heating : false);
-    const [parking, setParking] = useState<number>(nest ? nest.parking : 0);
-    const [rooms, setRooms] = useState<number>(nest ? nest.rooms : 0);
-    const [longitude, setLongitude] = useState<number>(nest ? nest.longitude : 0);
-    const [latitude, setLatitude] = useState<number>(nest ? nest.latitude : 0);
-    const [price, setPrice] = useState<number>(nest ? nest.price : 0);
-    const [userId, setUserId] = useState(nest ? nest.userId : '');
-    
-    const handleFormSubmit = (e:FormEvent) => {
-        e.preventDefault();
-
-        handleNestMutation({
-            _id: id,
-            title,
-            type,
-            size,
-            furnished,
-            dishwasher,
-            washingMachine,
-            dryer,
-            airConditioning,
-            heating,
-            parking,
-            rooms,
-            longitude,
-            latitude,
-            price,
-            userId,
-        });
-    };
+const TextInput = (props: HtmlInputProps) => {
+    const [field, meta] = useField({...props});
 
     return (
-        <div>
-            <form onSubmit={handleFormSubmit}>
-                <input type="hidden" value={id} onChange={e => setId(e.target.value)}/>
+        <>
+            {props.label && <label htmlFor={props.name}>{props.label}</label>}
+            <input {...field} {...props}/>
+            {meta.error && meta.touched && <span>{meta.error}</span>}
+        </>
+    );
+};
+
+const Checkbox = ({ label, ...props }: HtmlInputProps) => {
+    const [field, meta, helpers] = useField({...props, type: 'checkbox'});
+    
+    return (
+        <>
+            <label>
+                {label}
+                <input type="checkbox" {...field} {...props} />
+            </label>
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </>
+    );
+};
+  
+const NestForm: React.FC<NestFormProps> = ({handleNestMutation, nest}) => {
+
+    return(
+        <Formik
+            initialValues={{
+                id: nest ? nest._id : '',
+                title: nest ? nest.title : '',
+                type: nest ? nest.type : '',
+                size: nest ? nest.size : 0,
+                furnished: nest ? nest.furnished : false,
+                dishwasher: nest ? nest.dishwasher : false,
+                washingMachine: nest ? nest.washingMachine : false,
+                dryer: nest ? nest.dryer : false,
+                airConditioning: nest ? nest.airConditioning : false,
+                heating: nest ? nest.heating : false,
+                parking: nest ? nest.parking : 0,
+                rooms: nest ? nest.rooms : 0,
+                longitude: nest ? nest.longitude : 0,
+                latitude: nest ? nest.latitude : 0,
+                price: nest ? nest.price : 0,
+                userId: nest ? nest.userId : '',
+            }}
+            validationSchema= {Yup.object({
+                id: Yup.string(),
+                title: Yup.string().required('Please enter the title'),
+                type: Yup.string().required('Please enter the property type'),
+                size: Yup.number().required('Please enter the property type'),
+                furnished: Yup.boolean(),
+                dishwasher: Yup.boolean(),
+                washingMachine: Yup.boolean(),
+                dryer: Yup.boolean(),
+                airConditioning: Yup.boolean(),
+                heating: Yup.boolean(),
+                parking: Yup.number(),
+                rooms: Yup.number().required('Please enter the number of rooms'),
+                price: Yup.number().required('Please enter the monthly rent'),
+                userId: Yup.string(),
+            })}
+            onSubmit={(values) => {
+                handleNestMutation({
+                    _id: values.id,
+                    ...values
+                });
+            }}
+        >
+            <Form>
+                <TextInput type="hidden" name="id" id="id"/>
+
                 <div>
-                    <label htmlFor="title">Title</label>
-                    <input type="text" name="title" id="title" value={title} onChange={e => setTitle(e.target.value)}/>
+                    <TextInput type="text" name="title" id="title" label="Title"/>
                 </div>
                 <div>
-                    <label htmlFor="type">type</label>
-                    <input type="text" name="type" id="type" value={type} onChange={e => setType(e.target.value)}/>
+                    <TextInput type="text" name="type" id="type" label="Type"/>
                 </div>
                 <div>
-                    <label htmlFor="size">size</label>
-                    <input type="number" name="size" id="size" value={size} onChange={e => setSize(+e.target.value)}/>
+                    <TextInput type="number" name="size" id="size" label="Size"/>
                 </div>
                 <div>
-                    <label htmlFor="furnished">furnished</label>
-                    <input type="checkbox" name="furnished" id="furnished" checked={furnished} onChange={e => setFurnished(prevState => !prevState)} />
+                    <Checkbox name="furnished" id="furnished" label="Furnished"/>
                 </div>
                 <div>
-                    <label htmlFor="dishwasher">dishwasher</label>
-                    <input type="checkbox" name="dishwasher" id="dishwasher" checked={dishwasher} onChange={e => setDishwasher(prevState => !prevState)}/>
+                    <Checkbox name="dishwasher" id="dishwasher" label="Dishwasher"/>
                 </div>
                 <div>
-                    <label htmlFor="washingMachine">washingMachine</label>
-                    <input type="checkbox" name="washingMachine" id="washingMachine" checked={washingMachine} onChange={e => setWashingMachine(prevState => !prevState)}/>
+                    <Checkbox name="washingMachine" id="washingMachine" label="WashingMachine"/>
                 </div>
                 <div>
-                    <label htmlFor="dryer">dryer</label>
-                    <input type="checkbox" name="dryer" id="dryer" checked={dryer} onChange={e => setDryer(prevState => !prevState)} />
+                    <Checkbox name="dryer" id="dryer" label="Dryer"/>
                 </div>
                 <div>
-                    <label htmlFor="airConditioning">airConditioning</label>
-                    <input type="checkbox" name="airConditioning" id="airConditioning" checked={airConditioning} onChange={e => setAirconditioning(prevState => !prevState)} />
+                    <Checkbox name="airConditioning" id="airConditioning" label="AirConditioning"/>
                 </div>
                 <div>
-                    <label htmlFor="heating">heating</label>
-                    <input type="checkbox" name="heating" id="heating" checked={heating} onChange={e => setHeating(prevState => !prevState)} />
+                    <Checkbox name="heating" id="heating" label="Heating"/>
                 </div>
                 <div>
-                    <label htmlFor="parking">parking</label>
-                    <input type="number" name="parking" id="parking" value={parking} onChange={e => setParking(+e.target.value)} />
+                    <TextInput type="number" name="parking" id="parking" label="Parking"/>
                 </div>
                 <div>
-                    <label htmlFor="rooms">rooms</label>
-                    <input type="text" name="rooms" id="rooms" value={rooms} onChange={e => setRooms(+e.target.value)}/>
+                    <TextInput type="number" name="rooms" id="rooms" label="Rooms"/>
                 </div>
                 <div>
-                    <label htmlFor="longitude">longitude</label>
-                    <input type="number" name="longitude" id="longitude" value={longitude} onChange={e => setLongitude(+e.target.value)} />
+                    <TextInput type="number" name="longitude" id="longitude" label="Longitude"/>
                 </div>
                 <div>
-                    <label htmlFor="latitude">latitude</label>
-                    <input type="number" name="latitude" id="latitude" value={latitude} onChange={e => setLatitude(+e.target.value)} />
+                    <TextInput type="number" name="latitude" id="latitude" label="Latitude"/>
                 </div>
                 <div>
-                    <label htmlFor="price">price</label>
-                    <input type="number" name="price" id="price" value={price} onChange={e => setPrice(+e.target.value)} />
+                    <TextInput type="number" name="price" id="price" label="Price"/>
                 </div>
                 <div>
-                    <label htmlFor="userId">userId</label>
-                    <input type="text" name="userId" id="userId" value={userId} onChange={e => setUserId(e.target.value)} />
+                    <TextInput type="string" name="userId" id="userId" label="User Id"/>
                 </div>
                 <div>
                     <input type="submit" value="Add the nest"/>
                 </div>
-            </form>
-        </div>
-    )
-}
+            </Form>
+        </Formik>
+    );
+};
 
-export default NestForm
+export default NestForm;
