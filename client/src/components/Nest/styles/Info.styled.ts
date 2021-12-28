12f92e1fe502891be279f876c3f3daae5becc695
Jora <jora_a@outlook.com>
2021-12-28 11:48:22 +0330

    Add styles to Info

--- client/src/components/Nest/styles/Info.styled.ts
+++ client/src/components/Nest/styles/Info.styled.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Info = styled.div`
     border-radius: 20px;
-    padding: 0 .75em;
+    padding: .5em .75em;
     margin-top: -0.9em;
     background-color: rgba(255,255,255,1);
+    box-shadow: 0 -5px 7px -5px #666;
 `;
