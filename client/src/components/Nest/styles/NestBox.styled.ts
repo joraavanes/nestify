d38aa29d30fa75e1f0adddef63512955922479e3
Jora <jora_a@outlook.com>
2022-03-09 21:31:34 +0330

    Refine NestBox box shadow

--- client/src/components/Nest/styles/NestBox.styled.ts
+++ client/src/components/Nest/styles/NestBox.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const NestBox = styled.div`
     background-color: #fff;
-    box-shadow: 0 10px 10px -5px #c3c3c3;
+    box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 1px;
     border-radius: 1em;
     margin-bottom: 1em;
     overflow: hidden;
